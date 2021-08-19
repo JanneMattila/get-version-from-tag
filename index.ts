ca61c2a0f1f1291b797f73fbf6bfd520b7f168b7
Janne Mattila <janne.mattila@hotmail.fi>
2021-08-19 14:47:54 +0300

    Add simple implementation

---
+++ index.ts
@@ -0,0 +1,27 @@
+import * as core from "@actions/core";
+import { context } from "@actions/github";
+
+async function run() {
+    try {
+        if (context.eventName !== "release") {
+            core.setFailed(`Cannot execute release action in event '${context.eventName}'.`);
+            return;
+        }
+        
+        let tag = context.payload.release?.tag_name;
+        if (tag.length < 5) {
+            core.setFailed(`Invalid release tag. Expecting either 'v1.2.3' or '1.2.3' or 'v1.2.3-preview' but got '${tag}'.`);
+            return;
+        }
+
+        if (tag.startsWith("v")) {
+            tag = tag.substring(1);
+        }
+
+        core.setOutput("version", tag);
+    } catch (error) {
+        core.setFailed(error.message);
+    }
+}
+
+run();
